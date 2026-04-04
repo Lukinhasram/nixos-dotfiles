d75c315838fdffa815714038f2f70c36dadfc0d9
Lukinhasram <lucas.ramos@arapiraca.ufal.br>
2026-04-04 12:03:59 -0300

    initialize nixos-dotfiles with config submodule

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    quickshell = {
+      url = "github:outfoxxed/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.quickshell.follows = "quickshell";
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      specialArgs = { inherit inputs; };
+
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.lucas = import ./home.nix;
+            backupFileExtension = "backup";
+            extraSpecialArgs = { inherit inputs; };
+          };
+        }
+      ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,71 @@
+{ config, pkgs, inputs, ... }:
+
+let
+  dotfiles = "${config.home.homeDirectory}/nixos-dotfiles/config";
+  create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+  configs = {
+    nvim  = "nvim";
+    tmux  = "tmux";
+    niri  = "niri";
+    kitty = "kitty";
+    yazi  = "yazi";
+  };
+in
+
+{
+  home.username = "lucas";
+  home.homeDirectory = "/home/lucas";
+  home.stateVersion = "25.11";
+  programs.git = { 
+    enable = true; 
+    config = {
+      user.name = "Lukinhasram"; 
+      user.email = "lucas.ramos@arapiraca.ufal.br";
+      init.defaultBranch = "main";
+    };
+  };
+  programs.bash = {
+    enable = true;
+    initExtra = ''
+      export PS1="\[\e[32m\][\u\[\e[35m\]@\[\e[32m\]\h\e[35m\]:\[\e[34m\]\w\[\e[32m\]]\[\e[1;35m\]\$\[\e[0m\] "
+    '';
+    shellAliases = {
+      btw = "echo I use nixos, btw";
+      ll  = "ls -lah";
+      gs  = "git status -sb";
+      ga  = "git add";
+      gc  = "git commit";
+      v   = "nvim";
+      ta  = "tmux attach";
+      nr  = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#nixos";
+      nfu = "nix flake update";
+    };
+  };
+  
+  xdg.configFile = builtins.mapAttrs 
+    (name: subpath: {
+      source = create_symlink "${dotfiles}/${subpath}";
+      recursive = true;
+    })
+    configs;
+
+  home.packages = with pkgs; [
+    neovim
+    tmux
+    ripgrep
+    nil
+    nixpkgs-fmt
+    nodejs
+    gcc
+    fuzzel
+    swaybg
+    xwayland-satellite
+    gthumb
+    fastfetch
+    opencode
+    lua-language-server
+    yazi
+    fzf
+    inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.zotero
+  ];
+}
