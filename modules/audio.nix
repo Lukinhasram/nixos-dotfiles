6e9ad4a1e260bb216e377331de93bae24712ca10
Lukinhasram <lucas.ramos@arapiraca.ufal.br>
2026-04-06 19:26:59 -0300

    fix audio popping

--- modules/audio.nix
+++ modules/audio.nix
@@ -1,7 +1,29 @@
 { ... }:
 {
+  security.rtkit.enable = true;
+
   services.pipewire = {
     enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
+
+    wireplumber.extraConfig = {
+      "99-disable-suspend" = {
+        "monitor.alsa.rules" = [
+          {
+            matches = [ 
+              { "node.name" = "~alsa_input.*"; } 
+              { "node.name" = "~alsa_output.*"; } 
+            ];
+            actions = {
+              update-props = {
+                "session.suspend-timeout-seconds" = 0;
+              };
+            };
+          }
+        ];
+      };
+    };
   };
 }
