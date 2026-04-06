70734155a4f5bdb7e607b9b09b7df2382eece24e
Lukinhasram <lucas.ramos@arapiraca.ufal.br>
2026-04-06 20:31:22 -0300

    set ibus session vars for accented input fix

--- modules/locale.nix
+++ modules/locale.nix
@@ -1,8 +1,15 @@
 { ... }:
 {
   time.timeZone = "America/Maceio";
-
+  
+  i18n.inputMethod.enable = true;
+  i18n.inputMethod.type = "ibus";
   i18n.defaultLocale = "pt_BR.UTF-8";
+
+  environment.sessionVariables.GTK_IM_MODULE = "ibus";
+  environment.sessionVariables.QT_IM_MODULE = "ibus";
+  environment.sessionVariables.XMODIFIERS = "@im=ibus";
+
   console = {
     font = "Lat2-Terminus16";
     keyMap = "br-abnt2";
